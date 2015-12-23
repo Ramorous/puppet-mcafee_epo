6f5d38f17168b917f1e218ffdab51c13c8dd0508
Eric Bouliane <ebouliane@cahostrh001.hosting.deloitte.ca>
2015-12-23 09:36:39 -0500

    Initial init.pp modifications 	modified:   manifests/init.pp 	modified:   metadata.json

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,18 +1,15 @@
-# Class: mcafee-epo
+# Class: mcafee_epo
 #
-class mcafee-epo (
+class mcafee_epo (
   $class_enabled          = true,
   $agent_install_type     = 'script',
-  $agent_install_script   = 'puppet:///modules/mcafee-epo/installer.sh',
-  $agent_install_options  = '-i',
+  $agent_install_script   = 'puppet://moudules/files/install.sh',
+  $agent_install_options  = '-u',
   $agent_service_name     = 'cma',
   $agent_service_ensure   = 'running',
   $agent_service_enable   = true,
   $agent_service_provider = 'init',
-  $nails_service_name     = 'nails',
-  $nails_service_ensure   = 'running',
-  $nails_service_enable   = true,
-  $nails_service_provider = 'init',
+  $agent_service_script   = '/etc/init.d/cma',
 ) {
   # Verify if target host is Linux
   if $::kernel == 'Linux' {
@@ -23,22 +20,22 @@ class mcafee-epo (
       $class_enabled_real = $class_enabled
     }
     if ! is_string($agent_install_script) {
-      fail( 'agent_install_script variable is not a string')
+      fail( 'agent_install_script variable is not a string' )
     }
     if ! is_string($agent_install_type) {
-      fail( 'agent_install_type variable is not a string')
+      fail( 'agent_install_type variable is not a string' )
     }
-    if $agent_install_type.match(/(script|repository)/) == undef {
-      fail('agent_install_type must be: script/repository')
+    if ($agent_install_type != 'script') and ($agent_install_type != 'repository') {
+      fail( 'agent_install_type must be: script/repository' )
     }
     if ! is_string($agent_install_options) {
-      fail( 'agent_install_options variable is not a string')
+      fail( 'agent_install_options variable is not a string' )
     }
     if ! is_string($agent_service_name) {
-      fail( 'agent_service_name variable is not a string')
+      fail( 'agent_service_name variable is not a string' )
     }
     if ! is_string($agent_service_ensure) {
-      fail( 'agent_service_ensure variable is not a string')
+      fail( 'agent_service_ensure variable is not a string' )
     }
     if is_string($agent_service_enable) {
       $agent_service_enable_real = str2bool($agent_service_enable)
@@ -46,49 +43,44 @@ class mcafee-epo (
       $agent_service_enable_real = $agent_service_enable
     }
     if ! is_string($agent_service_provider) {
-      fail( 'agent_service_provider variable is not a string')
+      fail( 'agent_service_provider variable is not a string' )
     }
-    if ! is_string($nails_service_name) {
-      fail( 'nails_service_name variable is not a string')
-    }
-    if ! is_string($nails_service_ensure) {
-      fail( 'nails_service_ensure variable is not a string')
-    }
-    if is_string($nails_service_enable) {
-      $nails_service_enable_real = str2bool($nails_service_enable)
-    } else {
-      $nails_service_enable_real = $nails_service_enable
-    }
-    if ! is_string($nails_service_provider) {
-      fail( 'nails_service_provider variable is not a string')
+    if ! is_string($agent_service_script) {
+      fail( 'agent_service_script variable is not a string' )
     }
 
     # Check if Class is disabled (Helps to disable some hosts within Foreman/Satellite)
     if $class_enabled_real == true {
       case $agent_install_type {
         'script': {
+          $agent_install_script_real = '/tmp/McAfee-Install.sh'
+          file{ $agent_install_script:
+            path   => $agent_install_script_real,
+            ensure => present,
+            source => $agent_install_script,
+            mode   => '0700',
+            owner  => 'root',
+            group  => 'root',
+          }
+          exec{ $agent_install_script:
+            command => "$agent_install_script_real $agent_install_options",
+            onlyif  => "/usr/bin/test ! -f $agent_service_script",
+            user    => root,
+            path    => ['/usr/bin'],
+          }
         }
         'repository': {
           package{ $agent_service_name:
-            install_options => 
+            name => $agent_service_name,
+            ensure => 'present',
           }
         }
       }
       service{ $agent_service_name:
-        ensure => $agent_service_ensure,
-        enable => $agent_service_enable_real,
+        ensure   => $agent_service_ensure,
+        enable   => $agent_service_enable_real,
+        provider => $agent_service_provider,
       }
-      service{ $nails_service_name:
-        ensure => $nails_service_ensure,
-        enable => $nails_service_enable_real,
-      }
-#      $command_string = "/usr/local/avamar/etc/avagent.d register ${admin_server} ${server_domain}"
-#      exec { 'AVRegister':
-#        command => $command_string,
-#        user    => root,
-#        onlyif  => '/usr/bin/test ! -f /usr/local/avamar/var/avagent.cfg',
-#        path    => ['/usr/bin','/sbin'],
-#      }
     } else {
       notice('Class disabled.')
     }
@@ -96,3 +88,4 @@ class mcafee-epo (
     notice('System is not Linux based.')
   }
 }
+include mcafee_epo
